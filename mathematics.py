4b1c518579ad2b6925221d044f9429dd1c8bc561
ADITYA RAJ <gnsaddy@gmail.com>
2018-02-21 23:37:18 +0530

    Python offers modules like math and random to carry out different mathematics like trigonometry, logarithms, probability and statistics, etc.

---
+++ mathematics.py
@@ -0,0 +1,15 @@
+# Python offers modules like math and random to carry out different mathematics like trigonometry, logarithms, probability and statistics, etc.
+
+import math as m
+import random as r
+print(m.pi)  # sows the value of pi
+print(m.cos(m.pi))  # value of cos pi
+print(m.exp(1))
+print(m.log(2))
+
+print(m.factorial(5))  # factorial
+
+# random is used to generate random no.or alphabets
+x = ['a', 'b', 'c', 'd', 'e']
+print(r.shuffle(x))
+print(r.choice(x))
